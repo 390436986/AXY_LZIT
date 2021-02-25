b8bb1085436b312f1b2fa3ed3098f70d37d2d08d
Bystander <3276735002@qq.com>
2021-02-25 21:19:26 +0800

    Add files via upload

---
+++ 爱校园/CloseState.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 爱校园
+{
+    class CloseState
+    {
+        private int _Timeout;
+
+        /// <summary>
+        /// In millisecond
+        /// </summary>
+        public int Timeout
+        {
+            get
+            {
+                return _Timeout;
+            }
+        }
+
+        private string _Caption;
+
+        /// <summary>
+        /// Caption of dialog
+        /// </summary>
+        public string Caption
+        {
+            get
+            {
+                return _Caption;
+            }
+        }
+
+        public CloseState(string caption, int timeout)
+        {
+            _Timeout = timeout;
+            _Caption = caption;
+        }
+    }
+}
